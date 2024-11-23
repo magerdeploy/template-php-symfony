295b7857e9901a81e213e26eef0351d85c786fbe
Prasetyo Wicaksono <pras.wicak92@gmail.com>
2024-11-23 17:20:43 +0700

    chore: update template

--- scripts.php
+++ scripts.php
@@ -12,22 +12,24 @@ return function ($io): \Generator {
     );
     $version = 'php' . $version;
 
+    $image = $io->ask('Image Name', 'username/your-image');
     $host = $io->ask('Host', 'mager-symfony.com');
 
-    yield "sed -i 's/{\$input.host}/{$host}/g' mager.yaml";
-    yield "sed -i 's/{\$input.host}/{$host}/g' mager.dev.yaml";
+    yield <<<CMD
+        sed -i 's/{\$input.image}/{$image}/g' mager.yaml
+        sed -i 's/{\$input.host}/{$host}/g' mager.yaml
+        sed -i 's/{\$input.host}/{$host}/g' mager.dev.yaml
+        sed -i 's/{\$input.php-version}/{$version}/g' Dockerfile"
+    CMD;
 
-    yield "sed -i 's/{\$input.php-version}/{$version}/g' Dockerfile";
-
+    $composerCommand = <<<SCRIPT
+        composer require runtime/frankenphp-symfony
+        composer config --json extra.symfony.docker 'true'
+    SCRIPT;
     if (file_exists('./composer.json')) {
         try {
-            yield 'composer require runtime/frankenphp-symfony';
-            yield "composer config --json extra.symfony.docker 'true'";
+            yield $composerCommand;
         } catch (\Exception) {
-            $composerCommand = <<<SCRIPT
-            composer require runtime/frankenphp-symfony
-            composer config --json extra.symfony.docker 'true'
-            SCRIPT;
             $io->warning('Composer not installed yet, please run this following command manually');
             $io->writeln($composerCommand);
         }
